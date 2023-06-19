5cd726616a9c7662c009297b05bf7b558b433e66
Alberto Paim <119620474+AlbertoPaim@users.noreply.github.com>
2023-06-19 08:43:35 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
-
+const nextConfig = {
+  output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './app/image.ts',
+  },
+  
+}
+ 
+module.exports = nextConfig
 module.exports = nextConfig
